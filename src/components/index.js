bec2b34d4dea198696762e759e3fc833d9f356a8
Vladyslav Basalyha <100045506+VladBasalyha@users.noreply.github.com>
2023-02-13 23:37:53 +0200

    Revert "Merge branch 'main' of https://github.com/VladBasalyha/contacts-book-7"
    This reverts commit fbbf5b99912287594b4f04df4b5107522f45c56b, reversing
    changes made to 9f97bb89029ccd591a53e0f24c9ac1ac885c1de3.

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from './Contacts/Contacts';
-export * from './Filter/Filter';
-export * from './Phonebook/ContactForm';
-export * from './Section/Section';
--- src/index.js
+++ src/index.js
@@ -5,22 +5,10 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
-// Normalize style
-import GlobalStyle from './globalStyle';
-
-// Redux store
-import { persistor, store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
